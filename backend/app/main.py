ad08d6e5d40bc9683c88738256c8333ad8d31a26
Bhavika <bhavikagoyal08@gmail.com>
2026-06-24 20:57:31 +0530

    Fix CORS for vercel deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         'http://localhost:3000',
-        "http://127.0.0.1:3000",],
+        "http://127.0.0.1:3000",
+        "https://campus-nest-eight.vercel.app"],
     allow_credentials = True,
     allow_methods=["*"],
     allow_headers=["*"],)
